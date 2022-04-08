5cd5363411b29d5a94549d14e11380cf9047de14
Himanshu Tiwari <52146171+htiwari29@users.noreply.github.com>
2022-04-08 11:40:35 +0530

    Validating password by implementing hashing.

---
+++ Password-Hashing.py
@@ -0,0 +1,26 @@
+
+from tkinter import *
+import bcrypt
+
+def validate(entered_password):
+    hash = b'$2b$12$c0yvMNCYGxIdm.WAt42wd.cUydXSEjx22t0xYbtIsfIwWSjjlhh9C'
+    entered_password = bytes(entered_password, encoding='utf-8')
+    if bcrypt.checkpw(entered_password, hash):
+        print("Login Successful")
+
+    else:
+        print("Invalid password")
+
+
+
+root = Tk()
+root.geometry("300x300")
+
+password_entry = Entry(root)
+password_entry.pack()
+
+btn = Button(text="Validate", command=lambda: validate(password_entry.get()))
+btn.pack()
+
+root.mainloop()
+
