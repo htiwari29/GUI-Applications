eb9dbd6f1a265e2baf301a5838bbacd9d0716f03
Himanshu Tiwari <52146171+htiwari29@users.noreply.github.com>
2022-04-08 07:43:24 +0530

    Credit Card Validator using Luhn's Algorithm

---
+++ CreditCardValidator.py
@@ -0,0 +1,18 @@
+def CreditCardValidator(card_number):
+    def digits_of(n):
+        return [int(d) for d in str(n)]
+    digits = digits_of(card_number)
+    odd_digits = digits[-1::-2]
+    even_digits = digits[-2::-2]
+    checksum = 0
+    checksum += sum(odd_digits)
+    for d in even_digits:
+        checksum += sum(digits_of(d*2))
+    return checksum % 10
+
+
+if CreditCardValidator("4532015112830396")==0:
+    print('Valid')  
+
+else:
+    print('Invalid')
